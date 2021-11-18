bb8889db131e0770a70342734483c59450dc0058
MishraKushagra007 <92109463+MishraKushagra007@users.noreply.github.com>
2021-11-18 11:51:20 +0530

    Add files via upload

---
+++ kushagraalpha.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+int main()
+{
+char x;
+scanf("%c,"&x);
+switch(x=='a'|| x=='e'|| x=='i'|| x=='o'|| x=='u'|| x=='A' x=='E'|| x=='I'|| x=='O'|| x=='U')
+{
+case 1: printf("vowles");
+break;
+case 0: printf("consonat");
+break;
+}
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+)
+{
+case 1: printf("alphabates");
+break;
+
