5c64a887739ae02e0f9ff3fedc0d2ae04492438f
MishraKushagra007 <92109463+MishraKushagra007@users.noreply.github.com>
2021-11-18 11:52:18 +0530

    Add files via upload

---
+++ kushagra.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+int main()
+{
+int x;
+scanf("%d",&x);
+switch(x)
+{
+case 1: printf("sunday");
+break;
+case 2: printf("monday");
+break;
+case 3: printf("tuesday");
+break; 
+case 4: printf("wednesday");
+break;
+case 5: printf("thursday");
+break;
+case 6: printf("friday");
+break;
+case 7: printf("saturday");
+break;
+}
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+;
+
+
+
+
+
+
+
+
