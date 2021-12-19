639ab3ff67f3cbd4a930e1e2fbde721271edb79f
MishraKushagra007 <92109463+MishraKushagra007@users.noreply.github.com>
2021-12-19 16:39:53 +0530

    Add files via upload

---
+++ cal the total expenses.c
@@ -0,0 +1,20 @@
+# include<stdio.h>
+int main()
+{
+	int quty,dis;
+	float rate,tot;
+	printf("enter the quantity and rate");
+	scanf("%d,%f",&&quty,rate);
+	if(quty=>1000)
+	{
+	dis=10;
+}
+	else
+	{
+	
+	dis=0;
+	tot = (quty*rate)-(quty*rate*dis/100);
+	printf("total expenses= rs.%f\n",tot);
+}
+	return 0;
+}
