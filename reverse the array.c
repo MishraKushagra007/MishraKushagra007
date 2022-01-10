b5376b4799d23420170d5ba5a516e99b2c5dc0b8
MishraKushagra007 <92109463+MishraKushagra007@users.noreply.github.com>
2022-01-10 23:27:21 +0530

    Add files via upload

---
+++ reverse the array.c
@@ -0,0 +1,19 @@
+# include<stdio.h>
+int main()
+{
+	int a[9] ={11 , 12 , 13 , 14 , 15 , 16 , 17, 18 , 19};
+	int i;
+	for(i=0;i<=8;i++)
+		{
+	printf("%d  "  ,  a[i]);
+}
+printf("\n");
+for(i=8;i>=0;i--)
+{
+printf("%d  "  ,  a[i]);
+}
+return 0;
+
+	
+	
+}
